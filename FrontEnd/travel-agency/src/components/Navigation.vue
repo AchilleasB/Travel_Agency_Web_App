8925d440bd944f5bf2f2734d5ce4bfb16ebb5245
AchilleasB <90069683+AchilleasB@users.noreply.github.com>
2024-01-27 23:27:24 +0100

    modified nav for ham menu, needs improvement

--- FrontEnd/travel-agency/src/components/Navigation.vue
+++ FrontEnd/travel-agency/src/components/Navigation.vue
@@ -1,31 +1,48 @@
 <script setup>
+import { ref } from 'vue';
+
+const visible = ref(false);
 </script>
 
 <template>
   <header class="text-center py-3 mb-4 ">
     <div class="mb-3">
-      <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
+      <router-link to="/" href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
         <img alt="Vue logo" src="@/assets/MakLogo.svg" class="w-100" />
-      </a>
+      </router-link>
     </div>
     <nav class="navbar navbar-expand-md navbar-#e9c557 bg-#e9c557">
       <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample04"
-          aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" 
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarsExample03"
+                @click="visible=!visible">
           <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarsExample04">
+        <div class="navbar-collapse" :class="!visible?'collapse':''" id="navbarsExample03">
           <ul class="navbar-nav me-auto mb-2 mb-md-0">
-            <li><router-link to="/" class="nav-link px-2 ">Home</router-link></li>
-            <li><router-link to="/about" class="nav-link px-2">About us</router-link></li>
-            <li><router-link to="/destination" class="nav-link px-2">Destinations</router-link></li>
-            <li><router-link to="/category" class="nav-link px-2">Categories</router-link></li>
+            <li class="nav-item">
+              <router-link to="/" class="nav-link" @click="visible=!visible">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/about" class="nav-link" @click="visible=!visible">About us</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/destination" class="nav-link" @click="visible=!visible">Destinations</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/category" class="nav-link" @click="visible=!visible">Categories</router-link>
+            </li>
           </ul>
           <div class="col-md-3 text-end">
             <ul class="navbar-nav me-auto mb-2 mb-md-0">
-            <li><router-link to="/account" class="nav-link px-2 ">My account</router-link></li>
-            <li><router-link to="/reservation" class="nav-link px-2">Reservations</router-link></li>
+            <li class="nav-item">
+              <router-link to="/account" class="nav-link" @click="visible=!visible">My account</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/reservation" class="nav-link" @click="visible=!visible">Reservations</router-link>
+            </li>
           </ul>
           </div>
         </div>
@@ -39,24 +56,11 @@
   width: 100vw;
   height: 8vh;
   background: #ffbf00ac;
+  font-size: large;
   display: flex;
   justify-content: space-between;
-  align-items: stretch;
+  /* align-items: stretch; */
   padding: 0 20px;
-}
-
-.nav-link {
-  color: #000000;
-}
-
-.container-fluid {
-  max-width: 1200px;
-  margin: auto;
-}
-
-.row {
-  justify-content: space-between;
-  align-items: center;
 }
 
 @media (max-width: 680px) {
